3e4f8a7617abb81d2d120cb5c99a194d2c752e48
Himanshu Prabhaker <adzdrio@gmail.com>
2025-11-24 11:40:53 +0530

    second-commit

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ app.use('/api/admin', adminRoutes);
 
 app.get('/api/health', (req, res) => res.json({ ok: true }));
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
