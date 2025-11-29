9577aa6771e1927769fc068cc1e90be2baa4b657
Himanshu Prabhaker <adzdrio@gmail.com>
2025-11-29 12:56:27 +0530

    29-11-25 12:56 PM

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,37 @@
 const User = require('../models/User');
 const Transaction = require('../models/Transaction');
 
+// exports.me = async (req, res) => {
+//   const user = await User.findById(req.user._id)
+//     .populate("parentId")
+//     .lean();
+//   // console.log(user)
+//   res.json({ user });
+// };
 exports.me = async (req, res) => {
-  const user = await User.findById(req.user._id)
-    .populate("parentId")
-    .lean();
-  // console.log(user)
-  res.json({ user });
+  try {
+    // Get logged-in user
+    const user = await User.findById(req.user._id)
+      .populate("parentId")
+      .lean();
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // Count all users referred by this user
+    const totalReferrals = await User.countDocuments({ parentId: user._id });
+
+    // Count only users who have paid
+    const totalReferralsPaid = await User.countDocuments({ parentId: user._id, hasPaid: true });
+
+    // Add these counts to user object
+    user.totalReferrals = totalReferrals;        // all referred users
+    user.totalReferralsPaid = totalReferralsPaid; // only paid users
+
+    res.json({ user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 exports.leaderboard = async (req, res) => {
